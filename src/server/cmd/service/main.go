194cc46244b238a5a62b98a8a0c12fc475b3520e
Vadim Shashin <enapiuz@gmail.com>
2018-11-07 08:10:36 +0300

    Basic serve

--- src/server/cmd/service/main.go
+++ src/server/cmd/service/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+)
 
 func main() {
-	fmt.Println("Hello!")
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", http.StripPrefix("/", fs))
+
+	http.HandleFunc("*", serveTemplate)
+
+	log.Println("Listening...")
+	http.ListenAndServe(":8890", nil)
+}
+
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
+	lp := filepath.Join("static", "index.html")
+	fp := filepath.Join("static", filepath.Clean(r.URL.Path))
+
+	tmpl, _ := template.ParseFiles(lp, fp)
+	tmpl.ExecuteTemplate(w, "index", nil)
 }
